db5c3741a80e8c71b13b5adce46a432453fe6fe8
Mason Vines <mason.vines@gmail.com>
2015-08-10 22:37:43 -0400

    Updating some xml zipping.

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/data.xml "0.0.8"]
+                 [org.clojure/data.zip "0.1.1"]
                  [clj-http-lite "0.2.1"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]}}
   :plugins [[lein-midje "3.0.0"]])
--- src/bgg_parser/core.clj
+++ src/bgg_parser/core.clj
@@ -1,16 +1,30 @@
 (ns bgg-parser.core)
 
 (require '[clj-http.lite.client :as client]
-         '[clojure.xml :as xml]
-         '[clojure.zip :as zip]
-         '[clojure.java.io :as io])
+         '[clojure.xml :as c-xml]
+         '[clojure.data.xml :as c-d-xml :refer [parse]]
+         '[clojure.zip :as c-zip :refer [xml-zip]]
+         '[clojure.data.zip :as c-d-zip]
+         '[clojure.data.zip.xml :as c-d-z-xml :refer [xml-> xml1-> attr attr= text]]
+         '[clojure.java.io :as io]
+         '[clojure.pprint :as print])
 
-(def die-macher (client/get "http://www.boardgamegeek.com/xmlapi2/thing?id=1,type=boardgame" {:as :byte-array }))
+(defonce die-macher (client/get "http://www.boardgamegeek.com/xmlapi2/thing?id=1,type=boardgame" {:as :byte-array }))
 
 (defn zip-str [s]
-  (zip/xml-zip
-   (xml/parse (java.io.ByteArrayInputStream. (.getBytes s)))))
+  (c-zip/xml-zip
+   (c-xml/parse (java.io.ByteArrayInputStream. (.getBytes s)))))
 
-(def dm-tree (zip/xml-zip (xml/parse (java.io.ByteArrayInputStream. (:body die-macher)))))
+(def dm-tree (c-zip/xml-zip (c-xml/parse (java.io.ByteArrayInputStream. (:body die-macher)))))
 
-(zip/root dm-tree)
+
+(print/pprint (c-xml/parse (java.io.ByteArrayInputStream. (:body die-macher))))
+
+
+(print/pprint dm-tree)
+
+(def d-body (c-xml/parse (java.io.ByteArrayInputStream. (:body die-macher))))
+d-body
+
+(get-in d-body [:attrs :termsofuse])
+
